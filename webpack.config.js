ac227d3a625ff2e2d068f0f2af70c25a40289e66
ynonp <ynonperek@gmail.com>
2022-11-03 08:30:55 +0200

    convert project to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,20 @@ const path = require('path');
 module.exports = {
   context: __dirname,
   mode: "production",
-  entry: "./js/main.js",
+  devtool: 'inline-source-map',  
+  entry: "./js/main.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   output: {
     path: path.join(process.cwd(), 'js'),
     publicPath: "/js/",
